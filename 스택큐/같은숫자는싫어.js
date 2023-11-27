0a85e65c5687bf380404b80b71759750688ac912
WhyBusyy <ybg6152@naver.com>
2023-11-27 23:42:25 +0900

    2주차 스택큐 1번문제 완료

--- 스택큐/같은숫자는싫어.js
+++ 스택큐/같은숫자는싫어.js
@@ -11,10 +11,11 @@
 
 function solution(arr)
 {
-    var answer = [];
+    let answer = [arr[0]];
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-    
+    for(let i = 1; i < arr.length ; i++) {
+        if(arr[i-1] !== arr[i]) answer.push(arr[i]);
+    }
+
     return answer;
 }
