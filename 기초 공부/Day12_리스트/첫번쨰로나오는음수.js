2e90ba5b43e887e2cb77b3f16f1f8290db4d342d
WhyBusyy <ybg6152@naver.com>
2024-05-04 01:38:46 +0900

    Day12 2,3,4,5번문제

---
+++ 기초 공부/Day12_리스트/첫번쨰로나오는음수.js
@@ -0,0 +1,28 @@
+// 문제 설명
+// 정수 리스트 num_list가 주어질 때, 첫 번째로 나오는 음수의 인덱스를 return하도록 solution 함수를 완성해주세요. 음수가 없다면 -1을 return합니다.
+
+// 제한사항
+// 5 ≤ num_list의 길이 ≤ 100
+// -10 ≤ num_list의 원소 ≤ 100
+// 입출력 예
+// num_list	result
+// [12, 4, 15, 46, 38, -2, 15]	5
+// [13, 22, 53, 24, 15, 6]	-1
+// 입출력 예 설명
+// 입출력 예 #1
+
+// 5번 인덱스에서 음수가 처음 등장하므로 5를 return합니다.
+// 입출력 예 #2
+
+// 음수가 없으므로 -1을 return합니다.
+
+function solution(num_list) {
+  var answer = -1;
+  num_list.map((e, i) => {
+    if (answer == -1 && e < 0) answer = i;
+  });
+  return answer;
+}
+
+//findIndex
+const solution = (num_list) => num_list.findIndex((v) => v < 0);
