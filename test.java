a1ba55133882b1d9b5826683d756433b5e3bc547
WhyBusyy <ybg6152@naver.com>
2024-05-20 21:05:17 +0900

    자바 코드 확인

--- test.java
+++ test.java
@@ -1,29 +1,18 @@
-public class Soojebi{
-    public static int fn(int key, int[] arr, int cnt) {
-        int mid;
-        int low = 0, high = cnt -1;
-        int i=0;
-
-        while(low <= high){
-            i++;
-            mid = (low + high) / 2;
-
-            if(key == arr[mid]){
-                return i;
-            }
-            else if(key < arr[mid]) {
-                high = mid - 1;
-            }
-            else {
-                low = mid+1;
-            }
-        }
-        return -1;
+class Parent{
+    String name="Parent";
+    public Parent(){
+        System.out.print(this.name);
     }
+}
 
-    public static void main(String []args){
-        int[] a = {92,100,215,341,625,716,812,813,820,901,902};
-        int cnt = fn(92, a, 11);
-        System.out.println(cnt);
+class Child extends Parent{
+    public Child(String name){
+        System.out.print(this.name);
+    }
+}
+
+public class Soojebi{
+    public static void main(String[] args) {
+        Child c = new Child("Soojebi");
     }
 }
